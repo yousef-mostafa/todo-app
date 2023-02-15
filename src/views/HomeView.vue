1533786bd5109144a5ee4a7c384b6bc251f53be9
Yousef mostafa <yousef.moustafa@bevatel.com>
2023-02-15 16:44:05 +0200

    sass structure and bootstrap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// // @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  background-color: green;
+}
+</style>
